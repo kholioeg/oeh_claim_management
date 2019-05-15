3b3401e5d2fe0b57b1d3b00ff0d10a9e4907261a
sarahmohamed22 <sarahmohamed@alexu.edu.eg>
2019-05-15 11:07:11 +0200

    Add price_plan model and view

--- medical_insurance/__manifest__.py
+++ medical_insurance/__manifest__.py
@@ -27,6 +27,8 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+
+        'views/priceplan.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
