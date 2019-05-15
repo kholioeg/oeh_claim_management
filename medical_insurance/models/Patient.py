683dccea97d1f0ee18cde24b48aa543d919ffb9b
asmaasaeed057 <asmaasaeed057@gmail.com>
2019-05-16 01:06:54 +0200

    Modify in patient view & model

--- medical_insurance/models/Patient.py
+++ medical_insurance/models/Patient.py
@@ -4,7 +4,16 @@ from odoo import models, fields, api
 class Patient(models.Model):
     _name = 'medical_insurance.patient'
     _inherit = 'res.partner'
-    name = fields.Char(string='Name')
+
+    name = fields.Char(
+        track_visibility='onchange'
+    )
+    email = fields.Char(
+        track_visibility='onchange'
+    )
+    phone = fields.Char(
+        track_visibility='onchange'
+    )
     NID = fields.Char(string='NID')
     date_of_birth = fields.Date(string='Birth date')
     gender = fields.Selection([
@@ -12,7 +21,7 @@ class Patient(models.Model):
         ('female', "Female"),
     ])
 
-    mariage_state = fields.Selection([
+    marital_status = fields.Selection([
         ('single', "Single"),
         ('maried', "Maried"),
     ], default="single")
@@ -20,5 +29,5 @@ class Patient(models.Model):
     weight = fields.Float()
     height = fields.Float()
     status = fields.Boolean()
-    visit = fields.One2many('medical_insurance.visit', inverse_name="patient_id",ondelete="set null", string="visit")
+    visit = fields.One2many('medical_insurance.visit', inverse_name="patient_id",ondelete="set null", string="visit",required=True)
     price_plan = fields.Many2one('medical_insurance.price_plan', ondelete="set null", string="Price plan")
