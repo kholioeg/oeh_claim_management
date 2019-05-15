93f45fce2f1002ed133a16d134f00f6413b5d324
Youmna-abdalaziz <youmnaabdalaziz@gmail.com>
2019-05-15 11:17:28 +0200

    Add inherit product model

--- medical_insurance/models/ServiceLine.py
+++ medical_insurance/models/ServiceLine.py
@@ -4,12 +4,14 @@ from odoo import models, fields, api
 class ServiceLine(models.Model):
 
     _name = 'medical_insurance.service_line'
+    _inherit = "product.product"
+    _inherit = "product.template"
     product = fields.Char()
-    sale_price = fields.Integer()
-    vendor_price = fields.Integer()
-    patient_price = fields.Integer()
-    #price_plane = fields.Many2one('medical_insurance.PricePlan', ondelete='set null', string="PricePlan")
-    #visit = fields.One2many('medical_insurance.Visit', inverse_name='visit', ondelete='set null', string="Visit")
+    sale_price = fields.Float()
+    vendor_price = fields.Float()
+    patient_price = fields.Float()
+    price_plane = fields.Many2one('medical_insurance.price_plan', ondelete='set null', string="PricePlan")
+    #visit = fields.One2many('medical_insurance.visit', inverse_name='visit', ondelete='set null', string="Visit")
 
 
 
