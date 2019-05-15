19ee8da371b49ff79bec8a27700e448b590282f5
asmaasaeed057 <asmaasaeed057@gmail.com>
2019-05-15 14:14:40 +0200

    Add patient model & view

---
+++ medical_insurance/models/Patient.py
@@ -0,0 +1,24 @@
+from odoo import models, fields, api
+
+
+class Patient(models.Model):
+    _name = 'medical_insurance.patient'
+    #_inherit = 'res.partner'
+    first_name = fields.Char()
+    last_name = fields.Char()
+    NID = fields.Integer()
+    date_of_birth = fields.Date()
+    gender = fields.Selection([
+        ('male', "Male"),
+        ('female', "Female"),
+    ], default="Male")
+    mariage_state = fields.Selection([
+        ('single', "Single"),
+        ('maried', "Maried"),
+    ], default="single")
+    blood_group = fields.Char()
+    weight = fields.Float()
+    height = fields.Float()
+    status = fields.Boolean()
+    visit = fields.One2many('medical_insurance.visit', inverse_name="patient_id",ondelete="set null", string="visit")
+    price_plan = fields.Many2one('medical_insurance.price_plan', ondelete="set null", string="PricePlan")
