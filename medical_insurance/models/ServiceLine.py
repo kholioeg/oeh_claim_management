bf43eca0d5d079186687b22d15df5a709a65b932
Youmna-abdalaziz <youmnaabdalaziz@gmail.com>
2019-05-15 12:25:00 +0200

    Solve merge priceplan and serviceline views problems

--- medical_insurance/models/ServiceLine.py
+++ medical_insurance/models/ServiceLine.py
@@ -4,13 +4,13 @@ from odoo import models, fields, api
 class ServiceLine(models.Model):
 
     _name = 'medical_insurance.service_line'
-    _inherit = "product.product"
-    _inherit = "product.template"
+    #_inherit = "product.product"
+    #_inherit = "product.template"
     product = fields.Char()
     sale_price = fields.Float()
     vendor_price = fields.Float()
     patient_price = fields.Float()
-    price_plane = fields.Many2one('medical_insurance.price_plan', ondelete='set null', string="PricePlan")
+    price_plan = fields.Many2one('medical_insurance.price_plan', ondelete='set null', string="PricePlan")
     #visit = fields.One2many('medical_insurance.visit', inverse_name='visit', ondelete='set null', string="Visit")
 
 
