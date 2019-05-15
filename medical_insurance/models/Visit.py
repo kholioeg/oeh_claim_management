ea0514c2cae9369efd104b1664611d1497ee7670
nadaessa <nessam378@gmail.com>
2019-05-15 15:38:15 +0200

    Add date_of_visit to visit model

--- medical_insurance/models/Visit.py
+++ medical_insurance/models/Visit.py
@@ -4,18 +4,18 @@ class Visit(models.Model):
 
     _name = 'medical_insurance.visit'
 
-    # patient_id = fields.Many2one('medical_insurance.patient')
-    # price_plan = fields.Char(related='patient_id.price_plan', readonly=True)
-    # patient_status = fields.Char(related='patient_id.status', readonly=True)
-    # medical_center_id = fields.Many2one('medical_insurance.medical_center')
-    service_line_id = fields.Many2one('medical_insurance.service_line')
-    contribution_charge = fields.Float(related='service_line_id.vendor_price', readonly=True)
-    patient_charge = fields.Float(related='service_line_id.patient_price', readonly=True)
+    patient_id = fields.Many2one('medical_insurance.patient')
+    price_plan = fields.Char(related='patient_id.price_plan', readonly=True)
+    patient_status = fields.Char(related='patient_id.status', readonly=True)
+    medical_center_id = fields.Many2one('medical_insurance.medical_center')
+    service_line_id = fields.Many2one('medical_insurance.service_line', required=True)
+    contribution_charge = fields.Float(string='Contribution Charge', related='service_line_id.vendor_price', readonly=True)
+    patient_charge = fields.Float(string='Patient Charge', related='service_line_id.patient_price', readonly=True)
 
     plan_status = fields.Selection(selection=[
         ('valid', 'Valid'),
         ('not_valid', 'Not Valid')
-    ], default='valid',readonly=True)
+    ], default=is_valid ,readonly=True)
 
     state = fields.Selection([
         ('new', 'New'),
@@ -25,4 +25,10 @@ class Visit(models.Model):
         ('cancelled', 'Cancelled'),
     ], default='new', readonly=True)
 
-    visit_type = fields.Char()
+    visit_type = fields.Char(required=True)
+
+    date_of_visit = fields.Datetime(readonly=True ,default=lambda self: fields.datetime.now())
+
+
+
+
