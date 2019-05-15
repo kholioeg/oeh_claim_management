da9a6ca20c668692fae0a9be2adad59a843469b2
Youmna-abdalaziz <youmnaabdalaziz@gmail.com>
2019-05-15 15:17:27 +0200

    Change product field to name

--- medical_insurance/models/ServiceLine.py
+++ medical_insurance/models/ServiceLine.py
@@ -6,12 +6,12 @@ class ServiceLine(models.Model):
     _name = 'medical_insurance.service_line'
     #_inherit = "product.product"
     #_inherit = "product.template"
-    product = fields.Char()
+    name = fields.Char(string="Product")
     sale_price = fields.Float()
     vendor_price = fields.Float()
     patient_price = fields.Float()
     price_plan = fields.Many2one('medical_insurance.price_plan', ondelete='set null', string="PricePlan")
-    visit = fields.One2many('medical_insurance.visit', inverse_name='service_line_id', ondelete='set null', string="Visit")
+    visit = fields.One2many('medical_insurance.visit', inverse_name='service_line', ondelete='set null', string="Visit")
 
 
 
