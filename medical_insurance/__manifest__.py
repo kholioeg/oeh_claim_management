ecbc586b1019bc7cb721849536f503182127ecf8
nadaessa <nessam378@gmail.com>
2019-05-15 12:43:49 +0200

    Add visit model and view

--- medical_insurance/__manifest__.py
+++ medical_insurance/__manifest__.py
@@ -27,6 +27,8 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
+        'views/visit.xml',
+
     ],
     # only loaded in demonstration mode
     'demo': [
