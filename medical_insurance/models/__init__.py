dd99deeb432147871e05c03ad5fb5eab39905f00
Youmna-abdalaziz <youmnaabdalaziz@gmail.com>
2019-05-15 15:34:05 +0200

    Solve init.py conflict

--- medical_insurance/models/__init__.py
+++ medical_insurance/models/__init__.py
@@ -5,8 +5,4 @@ from . import Patient
 from . import Visit
 from . import ServiceLine
 from . import PricePlan
-<<<<<<< HEAD
-
-=======
 from . import MedicalCenter
->>>>>>> 4755322da36336fe3b22f58538e88a50ddf52d03
