31b9a7ea6ebd9bfc5c5907bda06e1645d0b59220
nadaessa <nessam378@gmail.com>
2019-05-16 09:46:42 +0200

    Edit in visit view

--- medical_insurance/models/Visit.py
+++ medical_insurance/models/Visit.py
@@ -7,19 +7,19 @@ class Visit(models.Model):
     price_plan = fields.Char(related='patient_id.price_plan.name', readonly=True)
     patient_status = fields.Boolean(related='patient_id.status', readonly=True)
     medical_center_id = fields.Many2one('medical_insurance.medical_center')
-    service_line_id = fields.Many2one('medical_insurance.service_line.name', required=True)
+    service_line_id = fields.Many2one('medical_insurance.service_line', required=True)
     contribution_charge = fields.Float(string='Contribution Charge', related='service_line_id.vendor_price', readonly=True)
     patient_charge = fields.Float(string='Patient Charge', related='service_line_id.patient_price', readonly=True)
-
-    # plan_status = fields.Selection(selection=[
-    #     ('valid', 'Valid'),
-    #     ('not_valid', 'Not Valid')
-    # ], default=lambda self: self.is_valid(), readonly=True)
 
     plan_status = fields.Selection(selection=[
         ('valid', 'Valid'),
         ('not_valid', 'Not Valid')
-    ], default='valid', readonly=True)
+    ], default=lambda self: self.is_valid(), readonly=True)
+
+    # plan_status = fields.Selection(selection=[
+    #     ('valid', 'Valid'),
+    #     ('not_valid', 'Not Valid')
+    # ], default='valid', readonly=True)
 
     state = fields.Selection([
         ('new', 'New'),
@@ -32,9 +32,10 @@ class Visit(models.Model):
     visit_type = fields.Char(required=True)
     date_of_visit = fields.Datetime(readonly=True, default=lambda self: fields.datetime.now())
 
-    # @api.model
-    # def is_valid(self):
-    #     if self.patient_status:
-    #         self.plan_status = 'valid'
-    #     else:
-    #         self.plan_status = 'not_valid'
+    @api.multi
+    def is_valid(self):
+        if self.patient_status:
+            self.plan_status = 'valid'
+        else:
+            self.plan_status = 'not_valid'
+
