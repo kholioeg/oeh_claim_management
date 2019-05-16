ab5eec46649bc4b5d97cb3dfc9b6f079b5e6420c
nadaessa <nessam378@gmail.com>
2019-05-16 10:51:47 +0200

    Edit in visit model

--- medical_insurance/models/Visit.py
+++ medical_insurance/models/Visit.py
@@ -3,25 +3,27 @@ from odoo import models, fields, api
 class Visit(models.Model):
     _name = 'medical_insurance.visit'
 
-    patient_id = fields.Many2one('medical_insurance.patient')
-    price_plan = fields.Char(related='patient_id.price_plan.name', readonly=True)
-    patient_status = fields.Boolean(related='patient_id.status', readonly=True)
-    medical_center_id = fields.Many2one('medical_insurance.medical_center')
-    service_line_id = fields.Many2one('medical_insurance.service_line', required=True)
-    contribution_charge = fields.Float(string='Contribution Charge', related='service_line_id.vendor_price', readonly=True)
-    patient_charge = fields.Float(string='Patient Charge', related='service_line_id.patient_price', readonly=True)
+    patient_id = fields.Many2one('medical_insurance.patient', string='Patient Name', required=True)
+    patient_status = fields.Boolean(string='Patient Status', related='patient_id.status', readonly=True)
+    medical_center_id = fields.Many2one('medical_insurance.medical_center', required=True)
+    price_plan = fields.Char(string='Price Plane', related='patient_id.price_plan.name', readonly=True)
+    service_line_id = fields.Many2one('medical_insurance.service_line', string='Service', required=True)
+    
+    # plan_status = fields.Selection(selection=[
+    #     ('valid', 'Valid'),
+    #     ('not_valid', 'Not Valid')
+    # ], default=lambda self: self.is_valid(), readonly=True)
 
     plan_status = fields.Selection(selection=[
         ('valid', 'Valid'),
         ('not_valid', 'Not Valid')
-    ], default=lambda self: self.is_valid(), readonly=True)
+    ], default='valid', readonly=True)
+    
+    visit_type = fields.Char(required=True)
+    contribution_charge = fields.Float(string='Contribution Charge', related='service_line_id.vendor_price', readonly=True)
+    patient_charge = fields.Float(string='Patient Charge', related='service_line_id.patient_price', readonly=True)
 
-    # plan_status = fields.Selection(selection=[
-    #     ('valid', 'Valid'),
-    #     ('not_valid', 'Not Valid')
-    # ], default='valid', readonly=True)
-
-    state = fields.Selection([
+    visit_state = fields.Selection([
         ('new', 'New'),
         ('confirmed', 'Confirmed'),
         ('inprogress', 'Inprogress'),
@@ -29,13 +31,12 @@ class Visit(models.Model):
         ('cancelled', 'Cancelled'),
     ], default='new', readonly=True)
 
-    visit_type = fields.Char(required=True)
     date_of_visit = fields.Datetime(readonly=True, default=lambda self: fields.datetime.now())
 
-    @api.multi
-    def is_valid(self):
-        if self.patient_status:
-            self.plan_status = 'valid'
-        else:
-            self.plan_status = 'not_valid'
+    # @api.multi
+    # def is_valid(self):
+    #     if self.patient_status:
+    #         self.plan_status = 'valid'
+    #     else:
+    #         self.plan_status = 'not_valid'
 
