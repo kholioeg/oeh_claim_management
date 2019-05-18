4ad772a0ebbb36121463d74982e18cd1b531f6d3
Youmna-abdalaziz <youmnaabdalaziz@gmail.com>
2019-05-18 14:54:40 +0200

    Modify in serviceline model

--- medical_insurance/models/ServiceLine.py
+++ medical_insurance/models/ServiceLine.py
@@ -4,15 +4,11 @@ from odoo import models, fields, api
 class ServiceLine(models.Model):
 
     _name = 'medical.insurance.service.line'
-    name = fields.Many2one('product.template', string="Product")
+    name = fields.Many2one('product.template',ondelete='set null', string="Product")
     sale_price = fields.Float()
     vendor_price = fields.Float()
     patient_price = fields.Float()
     price_plan = fields.Many2one('medical.insurance.price.plan', ondelete='set null', string="Price Plan")
     visit = fields.One2many('medical.insurance.visit', inverse_name='service_line_id', ondelete='set null',
                             string="Visit")
-
-
-
-
 
