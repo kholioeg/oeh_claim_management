7af7ed3e8761bf1c93fccab6d6ca5299db6e3573
Youmna-abdalaziz <youmnaabdalaziz@gmail.com>
2019-05-19 21:51:45 +0200

    Change sale_price value onchange of list_price value

--- medical_insurance/models/ServiceLine.py
+++ medical_insurance/models/ServiceLine.py
@@ -5,10 +5,15 @@ class ServiceLine(models.Model):
 
     _name = 'medical.insurance.service.line'
     name = fields.Many2one('product.template', string="Product")
-    sale_price = fields.Float()
+    sale_price = fields.Float(related="name.list_price")
     vendor_price = fields.Float()
     patient_price = fields.Float()
     price_plan = fields.Many2one('medical.insurance.price.plan', string="Price Plan")
     # visit = fields.One2many('medical.insurance.visit', inverse_name='service_line_id',
     #                         string="Visit")
     #
+
+
+@api.onchange('name.list_price')
+def onchange_field(self):
+    self.sale_price = self.name.list_price
