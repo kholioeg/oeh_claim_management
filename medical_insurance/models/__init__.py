9cb5a6f6079eac24e5a8e785c28d259d2047132f
Youmna-abdalaziz <youmnaabdalaziz@gmail.com>
2019-05-20 13:48:42 +0200

    Solve conflict

--- medical_insurance/models/__init__.py
+++ medical_insurance/models/__init__.py
@@ -8,7 +8,4 @@ from . import PricePlan
 from . import MedicalCenter
 from . import PatientLine
 from . import EHR
-<<<<<<< HEAD
-=======
 # from . import Disease
->>>>>>> 8f05fbeb9c7078e690fa8a9071f052b6c7bc62c3
