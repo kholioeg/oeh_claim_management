920e749bcf662320d0be6df7d3dac53776affb05
asmaasaeed057 <asmaasaeed057@gmail.com>
2019-05-20 16:30:44 +0200

    Add image field to patient model

--- medical_insurance/models/Patient.py
+++ medical_insurance/models/Patient.py
@@ -1,9 +1,11 @@
-from odoo import models, fields, api
+from odoo import models, fields, api,tools
 
 
 class Patient(models.Model):
     _name = 'medical.insurance.patient'
     _inherit = 'res.partner'
+
+    image = fields.Binary()
     NID = fields.Char(string='NID')
     date_of_birth = fields.Date(string='Birth date')
     gender = fields.Selection([
@@ -22,3 +24,10 @@ class Patient(models.Model):
     visit = fields.One2many('medical.insurance.visit', inverse_name="patient_id", ondelete="set null", string="visit",
                             required=True)
     EHR = fields.One2many('medical.insurance.ehr', inverse_name="patient_id", string="EHR", required=True)
+
+    # @api.multi
+    # @api.depends('image')
+    # def _get_image(self):
+    #     for rec in self:
+    #         rec.image = tools.image_resize_image_medium(
+    #             rec.image, size=(100, 100))
