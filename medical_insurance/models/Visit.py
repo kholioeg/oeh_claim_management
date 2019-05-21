137ca0a7589d196bbbdfc4dc18bde41a1d9f472b
nadaessa <nessam378@gmail.com>
2019-05-21 09:29:13 +0200

    Add relation bet visit and patientline and edit in status function in visit

--- medical_insurance/models/Visit.py
+++ medical_insurance/models/Visit.py
@@ -1,17 +1,18 @@
 from odoo import models, fields, api
 
-class Visit(models.Model):
 
+class Visit(models.Model):
     _name = 'medical.insurance.visit'
-    name = fields.Char(string="Claim No", readonly=True, required=True, copy=False, default='New')
-    patient_id = fields.Many2one('medical.insurance.patient', string='Patient Name', required=True)
-    price_plan = fields.Many2one(string='Price Plane', readonly=True)
-    patient_status = fields.Boolean(string='Patient Status', related='patient_id.status', readonly=True)
-    medical_center_id = fields.Many2one('medical.insurance.medical.center', required=True)
-    service_line_id = fields.Many2one('medical.insurance.service.line', string='Service', required=True)
-    contribution_charge = fields.Float(string='Contribution Charge', related='service_line_id.vendor_price', readonly=True)
-    patient_charge = fields.Float(string='Patient Charge', related='service_line_id.patient_price', readonly=True)
-    date_of_visit = fields.Datetime(default=lambda self: fields.datetime.now())
+
+    name = fields.Char(string="Claim No", readonly=True, required=True, copy=False, default='New', store='True')
+    patient_id = fields.Many2one('medical.insurance.patient.line', string='Patient Name', required=True, store='True')
+    price_plan = fields.Char(string='Price Plane', related='patient_id.price_plan.name', readonly=True, store=True)
+    patient_status = fields.Boolean(string='Patient Status', related='patient_id.status', readonly=True, store='True')
+    medical_center_id = fields.Many2one('medical.insurance.medical.center', required=True, store='True')
+    service_line_id = fields.Many2one('medical.insurance.service.line', string='Service', required=True, store='True')
+    contribution_charge = fields.Float(string='Contribution Charge', related='service_line_id.vendor_price', readonly=True, store='True')
+    patient_charge = fields.Float(string='Patient Charge', related='service_line_id.patient_price', readonly=True, store='True')
+    date_of_visit = fields.Datetime(default=lambda self: fields.datetime.now(), store='True')
     plan_status = fields.Char(compute='compute_plan_status', readonly=True)
     visit_type = fields.Selection([
         ('office', 'Office Visit'),
@@ -19,14 +20,14 @@ class Visit(models.Model):
         ('school', 'School/Sports Physical'),
         ('exam', 'Medicare Initial Preventative Physical Exam'),
         ('labs', 'Screening and Diagnostic labs'),
-    ], required=True)
+    ], required=True, store='True')
     visit_state = fields.Selection([
         ('new', 'New'),
         ('confirmed', 'Confirmed'),
         ('progress', 'In progress'),
         ('done', 'Done'),
         ('cancelled', 'Cancelled'),
-    ], default='new', readonly=True)
+    ], default='new', readonly=True, store='True')
 
 
     @api.model
@@ -38,17 +39,11 @@ class Visit(models.Model):
     @api.one
     def compute_plan_status(self):
         if self.patient_status:
-            print(self.patient_id)
-            for p in self.patient_id.price_plan.patient:
-                if p.name==self.patient_id.name:
-                    for med in self.medical_center_id.price_plan.medical_center_id:
-                        if med.name == self.medical_center_id.name:
-                            for s in self.service_line_id.price_plan.service_line:
-                                if s.name == self.service_line_id.name:
-                                    self.plan_status = 'Valid'
-                                    break
-                                else:
-                                    self.plan_status = 'Not Valid'
+            for med in self.patient_id.price_plan.medical_center_id:
+                if med.name == self.medical_center_id.name:
+                    for s in self.patient_id.price_plan.service_line:
+                        if s.name == self.service_line_id.name:
+                            self.plan_status = 'Valid'
                             break
                         else:
                             self.plan_status = 'Not Valid'
