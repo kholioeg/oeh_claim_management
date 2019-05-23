269939df4573902ad7f1622eb1c6dfbbc4e258fd
sarahmohamed22 <sarahmohamed@alexu.edu.eg>
2019-05-23 17:04:49 +0200

    Delete patient line model and its relations with other models

--- medical_insurance/models/Patient.py
+++ medical_insurance/models/Patient.py
@@ -21,6 +21,6 @@ class Patient(models.Model):
     status = fields.Boolean()
     visit = fields.One2many('medical.insurance.visit', inverse_name="patient_id", ondelete="set null", string="visit",
                             required=True)
-    patient_line = fields.One2many('medical.insurance.patient.line', inverse_name="patient_id", ondelete="set null", string="patient line",
+    patient_line = fields.One2many('medical.insurance.p', inverse_name="patient_id", ondelete="set null", string="patient line",
                             required=True)
     EHR = fields.One2many('medical.insurance.ehr', inverse_name="patient_id", string="EHR", required=True)
