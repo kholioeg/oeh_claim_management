a925659463397f4198d55f70b3f83fd81c7abfa3
nadaessa <nessam378@gmail.com>
2019-06-03 12:51:36 +0200

    Add security groups

--- medical_insurance/__manifest__.py
+++ medical_insurance/__manifest__.py
@@ -24,7 +24,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/medical_insurance_security.xml',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
         'views/patient.xml',
