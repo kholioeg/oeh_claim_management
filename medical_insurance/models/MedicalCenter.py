65c0e1c32601824099a7cfd2a9e2ef0d3f2854c0
nadaessa <nessam378@gmail.com>
2019-06-09 21:03:26 +0200

    Add relation between medical center and invoice

--- medical_insurance/models/MedicalCenter.py
+++ medical_insurance/models/MedicalCenter.py
@@ -3,7 +3,8 @@ from odoo import models, fields, api
 
 class MedicalCenter(models.Model):
     _name = 'medical.insurance.medical.center'
-    _inherit = 'res.partner'
+    _inherit = ['res.partner', 'account.invoice']
 
     price_plan_id = fields.Many2many('medical.insurance.price.plan','rel_price_plan', ondelete="set null", string="PricePlan")
     visit = fields.One2many('medical.insurance.claim', inverse_name="medical_center_id", string="visit")
+    medical_center_invoice = fields.Many2one('account.invoice', string="Invoice")
