5af72a51ae68ef5341949d0bc83d5ee455e3f8ff
asmaasaeed057 <asmaasaeed057@gmail.com>
2019-06-15 21:30:41 +0200

    Add update claim web route

--- medical_insurance/controllers/controllers.py
+++ medical_insurance/controllers/controllers.py
@@ -11,7 +11,8 @@ _logger = logging.getLogger(__name__)
 
 class MedicalInsurance(http.Controller):
 
-    @http.route('/medical_insurance/patients/', type='http', auth='public', method='GET')
+    #patient routes
+    @http.route('/api/medical_insurance/patients/', type='http', auth='public', method='GET')
     def medical_patient(self, **kw):
         patients = http.request.env['medical.insurance.patient']
         d = []
@@ -21,7 +22,7 @@ class MedicalInsurance(http.Controller):
         return json.dumps({'data': d})
 
 
-    @http.route('/medical_insurance/patient/',type='http', auth='public', method='GET')
+    @http.route('/api/medical_insurance/patient/',type='http', auth='public', method='GET')
     def patient_info_by_id(self, **kwargs):
         Patients = http.request.env['medical.insurance.patient']
         patient_info = []
@@ -51,47 +52,98 @@ class MedicalInsurance(http.Controller):
 
 
 
-    @http.route('/medical_insurance/services/', type='http', auth='public', method='GET')
-    def medical_patient(self, **kwargs):
-        patients = http.request.env['medical.insurance.patient']
-        d = []
-        patient = patients.sudo().search(['price_plan', '=', kwargs['price_plan']])
-        for x in patient:
-            d.append({'price_plan': x.price_plan})
-        return json.dumps({'data': d})
+    # create claim / api
+    @http.route('/api/medical_insurance/createclaim/', auth='public', methods=['POST'], type='json', csrf=False)
+    def apicreateclaim(self, **args):
+        data = request.httprequest.data
+        res = json.loads(data)
+        claim=request.env['medical.insurance.claim'].sudo().create({
+            'patient_id' : res['patient_id'],
+            'medical_center_id' : res['medical_center_id'],
+            'service_line_id' : res['service_line_id'],
+            'visit_type' : res['visit_type']
+        })
+
+        if claim.claim_status == 'Not Valid':
+            return {"response": 404}
+
+        return {'response': 200,
+                'name': claim.name,
+                'price plan': claim.price_plan,
+                'date of visit': claim.date_of_visit,
+                'patient status': claim.price_plan_status,
+                'contribution charge': claim.contribution_charge,
+                'patient charge': claim.patient_charge,
+                'claim status' :claim.claim_status,
+                'visit type': claim.visit_type,
+                'visit state': claim.visit_state,
+
+                }
+
+
+
+    #create claim / web
+
+    @http.route('/medical_insurance/claim/new/', type='http', auth="public", methods=['GET'], website=True)
+    def get_create_claim_form(self, **kw):
+        return http.request.render('medical_insurance.create', {})
+
+
+    @http.route('/medical_insurance/createclaim/',type='http', auth='public', methods=['POST'], csrf=False)
+    def webcreateclaim(self, **kw):
+        new_patient = {
+            'patient_id': kw['patient_id'],
+            'medical_center_id': kw['medical_center_id'],
+            'service_line_id': kw['service_line_id'],
+            'visit_type': kw['visit_type']
+        }
+        claim = request.env['medical.insurance.claim'].sudo().create(new_patient)
+        print(claim)
+        if claim.claim_status == 'Not Valid':
+            return http.request.render('medical_insurance.notFound', {})
+
+        return http.request.render('medical_insurance.claim_info', {
+            'claims': claim
+        })
+
+
+    # update claim / web
+
+    @http.route('/medical_insurance/claim/update/', type='http', auth="public", methods=['GET'], website=True)
+    def get_update_claim_form(self, **kwargs):
+        Claims = http.request.env['medical.insurance.claim']
+        claims = Claims.sudo().search([('name', '=', kwargs['cl'])])
+        return http.request.render('medical_insurance.update', {
+            'claims': claims
+        })
+
+
+    @http.route('/medical_insurance/updateclaim/', type='http', auth='public', methods=['POST'], csrf=False)
+    def webupdateclaim(self, **kw):
+        print(kw)
+        id=kw['patient_id']
+        update_patient = {
+            'patient_id': kw['patient_id'],
+            'medical_center_id': kw['medical_center_id'],
+            'service_line_id': kw['service_line_id'],
+            'visit_type': kw['visit_type']
+        }
+        cl=request.env['medical.insurance.claim'].sudo().write(update_patient)
+        print(cl)
 
 
 
 
-    # @http.route('/medical_insurance/createclaim/', auth='public', methods=['POST'], type='json', csrf=False)
-    # def createclaim(self, **args):
-    #     data = request.httprequest.data
-    #     res = json.loads(data)
-    #     claim=request.env['medical.insurance.claim'].sudo().create({
-    #         'patient_id' : res['patient_id'],
-    #         'medical_center_id' : res['medical_center_id'],
-    #         'service_line_id' : res['service_line_id'],
-    #         'visit_type' : res['visit_type']
-    #     })
-    #
-    #     if claim.claim_status == 'Not Valid':
-    #         return {"response": 404}
-    #
-    #     return {'response': 200,
-    #             'name': claim.name,
-    #             'price plan': claim.price_plan,
-    #             'date of visit': claim.date_of_visit,
-    #             'patient status': claim.price_plan_status,
-    #             'contribution charge': claim.contribution_charge,
-    #             'patient charge': claim.patient_charge,
-    #             'claim status' :claim.claim_status,
-    #             'visit type': claim.visit_type,
-    #             'visit state': claim.visit_state,
-    #
-    #             }
 
 
-
+    # @http.route('/medical_insurance/services/', type='http', auth='public', method='GET')
+        # def medical_patient(self, **kwargs):
+        #     patients = http.request.env['medical.insurance.patient']
+        #     d = []
+        #     patient = patients.sudo().search(['price_plan', '=', kwargs['price_plan']])
+        #     for x in patient:
+        #         d.append({'price_plan': x.price_plan})
+        #     return json.dumps({'data': d})
 
 
     # @http.route('/medical_insurance/updateclaim/', type='http', auth='public', method='POST')
@@ -116,54 +168,3 @@ class MedicalInsurance(http.Controller):
     #                   'contribution_charge' : claim.contribution_charge
     #                   })
     #     return json.dumps({'data': d})
-
-
-
-
-
-
-    #create claim
-
-    @http.route('/medical_insurance/claim/new', type='http', auth="public", methods=['GET'], website=True)
-    def claim_form(self, **kw):
-        return http.request.render('medical_insurance.create', {})
-
-
-    @http.route('/medical_insurance/createclaim/',type='http', auth='public', methods=['POST'], csrf=False)
-    def createclaim(self, **kw):
-        new_patient = {
-            'patient_id': kw['patient_id'],
-            'medical_center_id': kw['medical_center_id'],
-            'service_line_id': kw['service_line_id'],
-            'visit_type': kw['visit_type']
-        }
-        claim = request.env['medical.insurance.claim'].sudo().create(new_patient)
-
-        if claim.claim_status == 'Not Valid':
-            return http.request.render('medical_insurance.notFound', {})
-
-        return http.request.render('medical_insurance.claim_info', {
-            'claims': claim
-        })
-        # return {'response': 200,
-        #         'name': claim.name,
-        #         'price plan': claim.price_plan,
-        #         'date of visit': claim.date_of_visit,
-        #         'patient status': claim.price_plan_status,
-        #         'contribution charge': claim.contribution_charge,
-        #         'patient charge': claim.patient_charge,
-        #         'claim status': claim.claim_status,
-        #         'visit type': claim.visit_type,
-        #         'visit state': claim.visit_state,
-        #
-        #         }
-
-
-
-
-
-
-
-
-
-
