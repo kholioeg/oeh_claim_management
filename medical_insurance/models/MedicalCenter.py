4c89f3068c9619cb8c60cc7d25dd857b35f9b385
nadaessa <nessam378@gmail.com>
2019-06-17 16:45:24 +0200

    Remove deadly comments

--- medical_insurance/models/MedicalCenter.py
+++ medical_insurance/models/MedicalCenter.py
@@ -3,7 +3,6 @@ from odoo import models, fields, api
 
 class MedicalCenter(models.Model):
     _name = 'medical.insurance.medical.center'
-    _inherit = ['mail.thread', 'mail.activity.mixin']
     _inherits = {'res.partner': 'partner_id'}
 
     price_plan_id = fields.Many2many('medical.insurance.price.plan','rel_price_plan', ondelete="set null", string="PricePlan")
